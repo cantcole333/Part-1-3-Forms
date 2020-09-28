afdb83489cbabb96590638d67b6f2c9d22246cc2
Cole Cantwell <CantCole333@GoTVDSB.ca>
2020-09-28 09:22:10 -0400

    Layout adjustments

--- Part 1-3 Forms/Form1.Designer.cs
+++ Part 1-3 Forms/Form1.Designer.cs
@@ -42,27 +42,29 @@
             // 
             // txtInputName
             // 
-            this.txtInputName.Location = new System.Drawing.Point(0, 12);
+            this.txtInputName.Location = new System.Drawing.Point(214, 19);
+            this.txtInputName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtInputName.Name = "txtInputName";
-            this.txtInputName.Size = new System.Drawing.Size(181, 27);
+            this.txtInputName.Size = new System.Drawing.Size(159, 23);
             this.txtInputName.TabIndex = 0;
             this.txtInputName.TextChanged += new System.EventHandler(this.txtInputName_TextChanged);
             // 
             // lblInstructionName
             // 
             this.lblInstructionName.AutoSize = true;
-            this.lblInstructionName.Location = new System.Drawing.Point(0, 471);
+            this.lblInstructionName.Location = new System.Drawing.Point(12, 19);
             this.lblInstructionName.Name = "lblInstructionName";
-            this.lblInstructionName.Size = new System.Drawing.Size(166, 20);
+            this.lblInstructionName.Size = new System.Drawing.Size(133, 15);
             this.lblInstructionName.TabIndex = 1;
             this.lblInstructionName.Text = "Please enter your name:";
             this.lblInstructionName.Click += new System.EventHandler(this.label1_Click);
             // 
             // btnSubmit
             // 
-            this.btnSubmit.Location = new System.Drawing.Point(0, 444);
+            this.btnSubmit.Location = new System.Drawing.Point(360, 168);
+            this.btnSubmit.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnSubmit.Name = "btnSubmit";
-            this.btnSubmit.Size = new System.Drawing.Size(1517, 27);
+            this.btnSubmit.Size = new System.Drawing.Size(211, 20);
             this.btnSubmit.TabIndex = 3;
             this.btnSubmit.Text = "Click Me";
             this.btnSubmit.UseVisualStyleBackColor = true;
@@ -71,9 +73,9 @@
             // lblInstructionAge
             // 
             this.lblInstructionAge.AutoSize = true;
-            this.lblInstructionAge.Location = new System.Drawing.Point(0, 424);
+            this.lblInstructionAge.Location = new System.Drawing.Point(12, 79);
             this.lblInstructionAge.Name = "lblInstructionAge";
-            this.lblInstructionAge.Size = new System.Drawing.Size(154, 20);
+            this.lblInstructionAge.Size = new System.Drawing.Size(122, 15);
             this.lblInstructionAge.TabIndex = 4;
             this.lblInstructionAge.Text = "Please enter your age:";
             this.lblInstructionAge.Click += new System.EventHandler(this.label1_Click_1);
@@ -81,54 +83,55 @@
             // lblMessage
             // 
             this.lblMessage.AutoSize = true;
-            this.lblMessage.Location = new System.Drawing.Point(0, 404);
+            this.lblMessage.Location = new System.Drawing.Point(184, 259);
             this.lblMessage.Name = "lblMessage";
-            this.lblMessage.Size = new System.Drawing.Size(117, 20);
+            this.lblMessage.Size = new System.Drawing.Size(0, 15);
             this.lblMessage.TabIndex = 5;
-            this.lblMessage.Text = "jjjjjjjjjjjjjjjjjjjjjjjjjjj";
             this.lblMessage.Click += new System.EventHandler(this.lblMessageAge_Click);
             // 
             // lblInstrucionHeight
             // 
             this.lblInstrucionHeight.AutoSize = true;
-            this.lblInstrucionHeight.Location = new System.Drawing.Point(0, 384);
+            this.lblInstrucionHeight.Location = new System.Drawing.Point(12, 136);
             this.lblInstrucionHeight.Name = "lblInstrucionHeight";
-            this.lblInstrucionHeight.Size = new System.Drawing.Size(386, 20);
+            this.lblInstrucionHeight.Size = new System.Drawing.Size(307, 15);
             this.lblInstrucionHeight.TabIndex = 9;
             this.lblInstrucionHeight.Text = "Please enter your height in meters to two decimal places:";
             this.lblInstrucionHeight.Click += new System.EventHandler(this.lblInstrucionHeight_Click);
             // 
             // txtInputHeight
             // 
-            this.txtInputHeight.Location = new System.Drawing.Point(0, 357);
+            this.txtInputHeight.Location = new System.Drawing.Point(0, 168);
+            this.txtInputHeight.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtInputHeight.Name = "txtInputHeight";
-            this.txtInputHeight.Size = new System.Drawing.Size(1517, 27);
+            this.txtInputHeight.Size = new System.Drawing.Size(304, 23);
             this.txtInputHeight.TabIndex = 10;
             this.txtInputHeight.TextChanged += new System.EventHandler(this.txtInputHeight_TextChanged);
             // 
             // barAge
             // 
-            this.barAge.Location = new System.Drawing.Point(0, 301);
+            this.barAge.Location = new System.Drawing.Point(214, 79);
+            this.barAge.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.barAge.Maximum = 50;
             this.barAge.Name = "barAge";
-            this.barAge.Size = new System.Drawing.Size(1517, 56);
+            this.barAge.Size = new System.Drawing.Size(382, 45);
             this.barAge.TabIndex = 11;
             this.barAge.Scroll += new System.EventHandler(this.barAge_Scroll);
             // 
             // lblAge
             // 
             this.lblAge.AutoSize = true;
-            this.lblAge.Location = new System.Drawing.Point(0, 281);
+            this.lblAge.Location = new System.Drawing.Point(0, 211);
             this.lblAge.Name = "lblAge";
-            this.lblAge.Size = new System.Drawing.Size(0, 20);
+            this.lblAge.Size = new System.Drawing.Size(0, 15);
             this.lblAge.TabIndex = 12;
             this.lblAge.Click += new System.EventHandler(this.lblAge_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1594, 518);
+            this.ClientSize = new System.Drawing.Size(608, 324);
             this.Controls.Add(this.lblAge);
             this.Controls.Add(this.barAge);
             this.Controls.Add(this.txtInputHeight);
@@ -138,6 +141,7 @@
             this.Controls.Add(this.btnSubmit);
             this.Controls.Add(this.lblInstructionName);
             this.Controls.Add(this.txtInputName);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Message";
             this.Load += new System.EventHandler(this.Form1_Load);
